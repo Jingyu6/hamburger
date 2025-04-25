22977c352e47ad0f9bd562f3f838d5b4fa0c81d5
Jingyu6 <liujingyu@student.ethz.ch>
2025-04-24 17:06:37 -0700

    Added huggingface to api

---
+++ eval/launch_server.sh
@@ -0,0 +1,20 @@
+
+case "$1" in
+    "hf")
+        python -m eval.server \
+            --model_name meta-llama/Llama-3.2-1B-Instruct \
+            --model_type hf
+        ;;
+    "m2d")
+        python -m eval.server \
+            --model_name /data/data_persistent1/jingyu/m2d/ckpts/m2d-llama-1B-code-math-skip-finish.ckpt \
+            --model_type m2d
+        ;;
+    "blt")
+        echo ok
+        ;;
+    *)
+        echo Invalid choice of model type. 
+        exit 1
+        ;;
+esac
---
+++ eval/server.py
@@ -0,0 +1,90 @@
+import argparse
+
+import litserve as ls
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+from m2d.config import GenConfig
+from m2d.model.llama import M2DLlama
+
+
+class ModelLitAPI(ls.LitAPI):
+    def __init__(
+        self, 
+        model_name: str, 
+        model_type: str, 
+        device: str, 
+        **kwargs
+    ):
+        self.model_name = model_name
+        self.model_type = model_type
+        self.device = device
+        
+    def setup(self, device):
+        if self.model_type == "hf":
+            model = AutoModelForCausalLM.from_pretrained(
+                self.model_name, 
+                trust_remote_code=True, 
+                torch_dtype=torch.bfloat16, 
+                attn_implementation="flash_attention_2",
+                device_map=self.device, 
+            )
+            tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+            tokenizer.pad_token = tokenizer.eos_token
+            self.model = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+        elif self.model_type == "m2d":
+            self.model: M2DLlama = M2DLlama.load_from_checkpoint(self.model_name).to(self.device)
+        elif self.model_type == "blt":
+            raise NotImplemented
+
+    def predict(self, conversation, context):
+        max_gen_len = context.get("max_completion_tokens", None)
+        if max_gen_len is None:
+            max_gen_len = context.get("max_tokens", 128)
+        
+        # filter non essential fields
+        filtered_conversation = [
+            {"role": turn["role"], "content": turn["content"]}
+            for turn in conversation
+        ]
+
+        # generate based on different model type
+        if self.model_type == "hf":
+            output = self.model(
+                filtered_conversation, 
+                max_new_tokens=max_gen_len
+            )[0]["generated_text"][1]["content"]
+        elif self.model_type == "m2d":
+            gen_config = GenConfig()
+            gen_config.max_gen_len = max_gen_len
+            output = self.model.generate(
+                conversation=filtered_conversation, 
+                config=gen_config
+            )["output"]
+            self.model.report.get_speedup()
+        elif self.model_type == "blt":
+            raise NotImplemented
+        
+        yield output
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default="meta-llama/Meta-Llama-3.1-8B-Instruct")
+    parser.add_argument('--model_type', type=str, default="hf", choices=["hf", "m2d", "blt"])
+    parser.add_argument('--port', type=int, default=8000)
+    parser.add_argument('--device', type=str, default='cuda')
+
+    args = parser.parse_args(args)
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    api = ModelLitAPI(**vars(args))
+    server = ls.LitServer(
+        api, 
+        devices=1, 
+        spec=ls.OpenAISpec()
+    )
+    server.run(port=8000)
